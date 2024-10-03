7ec6ac77c922671dc2e89b0e40f2e405c966d87e
Nicolas Dobbeck Mendes <98404627+NicolasDobbeck@users.noreply.github.com>
2024-10-02 22:59:56 -0300

    First commit

---
+++ RestAPI/src/main/java/br/com/fiap/HelloWorldApi.java
@@ -0,0 +1,26 @@
+package br.com.fiap;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/rest")
+public class HelloWorldApi {
+
+    @GET
+    @Path("/v1")
+    public String hello(){
+        return "{\"mensagem\": \"Hello World\"}";
+    }
+
+    @GET
+    @Path("/v2")
+    public String helloV2(){
+        return """
+                {
+                    "mensagem": "Hello World"
+                }
+                """;
+    }
+
+}
